f5355f6030bbc2deb5cb2377582828d129a311b1
katyjohn124 <148299728+katyjohn124@users.noreply.github.com>
2024-01-13 19:44:22 +0800

    jsx

--- index.js
+++ index.js
@@ -19,21 +19,42 @@
 //add a class
 // ReactDOM.render(<h1 className="header">this is a big deal,huh?</h1>, document.getElementById('root'))
 
+
+
+
+// const production = (
+//     <nav>
+//         <h1>John's quickshop</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
+
+// ReactDOM.render(
+//     production,
+//     document.getElementById('root')
+// )
+
 //jsx
 
-
-const production = (
-    <nav>
-        <h1>John's quickshop</h1>
+const page = (
+    <div>
+        <img src="images\下载 (2).png" width="40px" />
+        <h1>Fun Facts About React</h1>
         <ul>
-            <li>Pricing</li>
-            <li>About</li>
-            <li>Contact</li>
+            <li>omg</li>
+            <li>oops!</li>
+            <li>really huh?</li>
+            <li>are u sure?</li>
+            <li>come on!</li>
         </ul>
-    </nav>
+    </div>
 )
 
 ReactDOM.render(
-    production,
+    page,
     document.getElementById('root')
 )
